a5f4e7acd24fad8d6f575a24e03206060e030fe5
ginnac <ginnacampbell1990@gmail.com>
2019-05-18 22:10:03 -0600

    brainstorming

---
+++ assets/Javascript/game.js
@@ -0,0 +1,19 @@
+// add any key to get started//
+    //Variables?? Array??//
+        //loop
+    //conditional
+    //call function
+
+//player wins when//
+    //Array of name of song??
+        //maybe Array for letters of each song name??
+    //loop for the song array
+    //function to call (choose) a song 
+//And look the array with letters of the song chosen
+    //loop for the song letters array 
+    //function to print correct letters in the right placement
+
+
+
+
+ 
