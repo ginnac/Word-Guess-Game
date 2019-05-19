ac4a3209d017262cc7e3fe7d77f8630860780d96
ginnac <ginnacampbell1990@gmail.com>
2019-05-19 14:48:10 -0600

    starting to apply some arrays and functions

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,13 +1,32 @@
 // add any key to get started//
+document.getElementById("start game").onkeyup= function(){getStarted()};
+
+function getStarted(){
+document.getElementById("start").innerHTML = "Game has started!";
+}
+// Var X = getStarted...
+
     //Variables?? Array??//
+
         //loop
     //conditional
     //call function
 
 //player wins when//
     //Array of name of song??
+    var songs = ["Despacito", "Firework", "Feels"];
         //maybe Array for letters of each song name??
+        var despacito = ["D","e","s","p","a","c","i","t","o"];
+        var firework = ["F","i","r","e","w","o","r","k"];
+        var feels = ["F","e","e","l","s"];
+        
+    
     //loop for the song array
+    for (i = 0; i <songs.length; i++) {
+        
+      }
+      document.getElementById("demo").innerHTML = text;
+
     //function to call (choose) a song 
 //And look the array with letters of the song chosen
     //loop for the song letters array 
