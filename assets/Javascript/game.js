ccf9db24ab6e8b7f8e57acadb42d17375564dde5
ginnac <ginnacampbell1990@gmail.com>
2019-05-25 18:26:33 -0600

    some CSS and  points counter working

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,115 +1,126 @@
 
 
-//-Array of name of song??
-    var songs = ["despacito", "firework", "feels"];
-    var guesssesremaining = 10;
-    var correctGuesses =[]
-    var incorrectGuesses = []
-    var div =""
-    var allGuesses =[]
+//Arrays: 
 
-    
-
-//Function to randomly choose an index from my array
-
- var randomSong= songs[Math.floor(Math.random()*songs.length)];
-
-        //printing to the console (testing)
-
-        console.log(randomSong);
+var songs = ["despacito", "firework", "feels"];
+var guessesremaining = 7;
+var correctGuesses =[]
+var incorrectGuesses = []
+var div =""
+var allGuesses =[]
+var points= 0;
+let uniqueArray = []
 
 
-//Loop to create underscores instead of letters, and be pushed to createUnderscore array so we can display the array.
 
-// function createUnderscores(){
-    for (var i=0;i<randomSong.length;i++) {
-             // lines.push(" _ ");
-           //div += lines[i];
-    div += "<span data-name =" +  randomSong.charAt(i) + ">" + " _ " +"</span>"; }
+//Math.random applied to randomly choose an index from my array
+var randomSong= songs[Math.floor(Math.random()*songs.length)];
 
-//printing the _ in the DOM
-//document.getElementById("word").innerHTML = div;
+//Loop to create underscores instead of letters, 
+for (var i=0;i<randomSong.length;i++) {
+    div += "<span data-name =" +  randomSong.charAt(i) + ">" + "   _   " +"</span>"; }
+
+//printing the Underscores in the DOM
 document.getElementById("word").innerHTML = div;
-    //underscores = "<div" +  randomSong.charAt(i) + ">" + " _ " +"</div>";
-
-    //console.log(lines);
-
-// taking guesses from user and accepting true values 
-
-document.addEventListener("keypress", function(event){
-    var code = event.keyCode 
-    var letter = String.fromCharCode(code);
-    var receivedGuess =letter.toLowerCase();
-
-    //printing to the console (testing we are receiving user input (keys))   
-    console.log(receivedGuess);
-
-   
-
-// comparing input with randomSong.charAt(i)... 
-
-    for(var i=0; i< randomSong.length; i++){
-    
-        if(receivedGuess === randomSong.charAt(i) && !allGuesses.includes(receivedGuess)) {
-           
-           // correctGuesses.push("");
-            console.log(randomSong[i]);
-
-            // printing each randomSong. chartAt(i) it in the Dom..
-
-            var list = document.getElementById("word");
-            console.log(list);
-           list.getElementsByTagName("span")[i].innerHTML = randomSong.charAt(i); 
-
- // creating an array with correct guesses          
-        // if(!correctGuesses.includes(receivedGuess)){
-            //if(correctGuesses.includes(randomSong.charAt(i)))
-             correctGuesses.push(randomSong.charAt(i));
-          // }
-
-console.log(correctGuesses);
 
 
-var guessL = correctGuesses.join("");
+// taking key guesses from user 
 
-console.log(guessL);
+document.addEventListener("keypress", function key(event){
+var code = event.keyCode 
+var letter = String.fromCharCode(code);
+var receivedGuess =letter.toLowerCase();
 
-//confirm the user guessed all letters - 
-if(randomSong === guessL){
-    alert("winner")}
+
+
+                            //printing to the console (testing we are receiving user input (keys))   
+                                        console.log(receivedGuess);
+
+// comparing input keys with letters from randomSong (using charAt(i) to do so...) 
+// loop to work with every letter from randomSong -
+for(var i=0; i< randomSong.length; i++){
+    // conditional used to compare just matching letters and include them once - 
+    if(receivedGuess === randomSong.charAt(i) && !allGuesses.includes(receivedGuess)) {
+       
+                             // testing randomSong[i]   
+                                    console.log(randomSong[i]);
+
+// printing it in the Dom each randomSong letter that matches with the user key input  ..
+
+        var list = document.getElementById("word");
+       list.getElementsByTagName("span")[i].innerHTML = randomSong.charAt(i); 
+
+// creating an array with correct guesses letters       
+         correctGuesses.push(randomSong.charAt(i));
+                                
+                                //testing - looking inside the correctGuesses array - 
+                                    console.log(correctGuesses);
+///..........
+
+}
+
+}
+
+allGuesses.push(receivedGuess);
+
+console.log(allGuesses)
+
+//Getting just incorrect letters...
+
+if(!correctGuesses.includes(receivedGuess)&& !incorrectGuesses.includes(receivedGuess)){
+incorrectGuesses.push(receivedGuess);
+
+//testing that just incorrect letters are being pushed....
+console.log(incorrectGuesses);
+
+// getting remaining guesses updated..
+guessesremaining = guessesremaining - 1;
+
+//testing remaining guesses counts down...
+console.log(guessesremaining);
+
+if(guessesremaining <= 0){
+alert("Game Over");
+//reloading page to let user start again...
+window.location.reload();
+}
+
+}
+
+// Getting points counter up to work...
+
+if(randomSong.length === correctGuesses.length){
+points = points + 1;
+alert("You Won!");
+window.location.reload();
+
 
 }
 
 
 
-else { incorrectGuesses.push(receivedGuess);
-//console.log(incorrectGuesses);
-document.getElementById("wronglettersguessed").innerHTML =  incorrectGuesses
+//printing incorrect letters..
+
+document.getElementById("incorrectLettersGroup").innerHTML = incorrectGuesses;
+
+//printing guessesremaing(and updates)
+
+document.getElementById("guessesRemaining").innerHTML = guessesremaining;
+
+//printing points (and updates)
+
+document.getElementById("pointsCounter").innerHTML = points;
+
+
+
 }
 
-
-   
-
-    
-
-
-
-            
-
-
-
-    }
-
-    allGuesses.push(receivedGuess);
-}
 );
 
 
 
 
 
-
-     
 
 
 
